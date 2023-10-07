2aaea4c6ab94f64c45cd7b4f02657c7693f4711f
abel2319 <abelbakary@gmail.com>
2023-10-07 02:44:28 +0100

    feat(task5): task 5 ✅

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module for summing up a list """
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ Sum the list of floats
+    Agrs:
+        input_list: [float]
+    Return: sum as a float.
+    """
+    return sum(input_list)
