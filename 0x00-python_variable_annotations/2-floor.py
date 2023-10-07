0ddab7eafc759a0a4d927586255ac745c97d2331
abel2319 <abelbakary@gmail.com>
2023-10-07 02:33:33 +0100

    feat(task2): task 2 ✅

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Floor Module"""
+import math
+
+
+def concat(n: float) -> int:
+    """ The floor of the floa
+    Agrs:
+        n: float
+    Return: floor(n)
+    """
+    return math.floor(n)
