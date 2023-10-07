1ae9180b44e42f21fb69a37d3cd0df0fa52e7169
abel2319 <abelbakary@gmail.com>
2023-10-07 02:30:46 +0100

    feat(task1): task 1 ✅

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Concat Module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Returns sum of two strings
+    Agrs:
+        str1: string
+        str2: string
+    Return: str1 + str2
+    """
+    return str1 + str2
