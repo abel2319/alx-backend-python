2e32eb6110bda4d1296a8fec1856e13b20b89e86
abel2319 <abelbakary@gmail.com>
2023-10-07 02:25:09 +0100

    feat(task0): task 0 ✅

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Add module
+"""
+
+
+def add(a: int, b: int) -> int:
+    """Add integer
+    Args:
+        a: integer
+        b: integer
+    returns: integer a + b
+    """
+    return a + b
